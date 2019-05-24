6c5af8472fe9cc5540292882538ac26d430b44e9
akira-cn <akira.cn@gmail.com>
2019-05-24 19:14:50 +0800

    update graph lib

--- examples/graph-line.glsl
+++ examples/graph-line.glsl
@@ -19,23 +19,22 @@ void main()
   // gl_FragColor = vec4(abs(sin(dd_time)),0.0,0.0,1.0);
   // vec2 st = gl_FragCoord.xy / dd_resolution;
 	// gl_FragColor = vec4(st, 0.0, 1.0);
-  vec2 grid = vec2(100, 100);
+  vec2 grid = vec2(10, 10);
 	vec2 st = gl_FragCoord.xy / dd_resolution;
   vec2 idx = grid_index(st, grid);
   
   st = grid_xy(st, grid);
   st = lerp(st, vec2(-10, -10), vec2(10, 10));
 
-  // float r = - PI * dd_time * 0.2 * (idx.x * 5.0 + idx.y + 1.0);
-  // mat2 m = mat2(sin(r), cos(r), cos(r), -sin(r));
+  float r = - PI * dd_time * 0.2 * (idx.x * 5.0 + idx.y + 1.0);
+  mat2 m = mat2(sin(r), cos(r), cos(r), -sin(r));
 
-  // st = m * st;
-  float pct = circle(st, vec2(0, 0), 8.0);
+  st = m * st;
 
-  // vec3 color = pct * vec3(1.0, 1.0, 1.0);
+  vec3 color = line_seg(st, vec2(-3.0, -3.0), vec2(3.0, 3.0), 1.0) * vec3(1.0, 1.0, 1.0);
 
   // float t = dd_time;
-  vec3 color = pct * random_color(idx + dd_randseed);
+  // vec3 color = pct * random_color(idx + dd_randseed);
   // vec3 color = random_color(idx + dd_randseed);
 	gl_FragColor = vec4(color, 1.0);
 }
--- src/graph.glsl
+++ src/graph.glsl
@@ -2,10 +2,10 @@
 
 #define M_GRAPH
 
-#define PLOT(f, st, step, th) plot_v(plot_distance(st, vec2(st.x - step, f(st.x - step)), vec2(st.x, f(st.x)), vec2(st.x + step, f(st.x + step))), th)
+#define PLOT(f, st, step, th) plot_d(plot_distance(st, vec2(st.x - step, f(st.x - step)), vec2(st.x, f(st.x)), vec2(st.x + step, f(st.x + step))), th)
 
 // Get the distance that point between lineSeg(v1, v2)
-float point_distance(in vec2 p, in vec2 v1, in vec2 v2) {
+float seg_distance(in vec2 p, in vec2 v1, in vec2 v2) {
   vec2 pd = p - v1;
   vec2 pd2 = p - v2;
   vec2 seg = v2 - v1;
@@ -20,21 +20,26 @@ float point_distance(in vec2 p, in vec2 v1, in vec2 v2) {
 }
 
 float plot_distance(in vec2 p, in vec2 v1, in vec2 v2, in vec2 v3) {
-  float d1 = point_distance(p, v1, v2);
-  float d2 = point_distance(p, v2, v3);
+  float d1 = seg_distance(p, v1, v2);
+  float d2 = seg_distance(p, v2, v3);
 
   return min(d1, d2);  
 }
 
 // Plot a line on Y using a value between 0.0 - 1.0
-float plot_v(in float v, in float thick){
-  float smth = min(0.618 * thick, 3.0);
-  return  smoothstep(-thick, -smth, v) - smoothstep(smth, thick, v);
+float plot_d(in float v, in float thick){
+  float smth = min(0.1 * thick, 0.5);
+  return smoothstep(-thick, smth - thick, v) - smoothstep(thick - smth, thick, v);
 }
 
 float line_seg(in vec2 st, in vec2 v1, in vec2 v2, in float th) {
-  float y = point_distance(st, v1, v2);
-  return plot_v(y, th);
+  float y = seg_distance(st, v1, v2);
+  return plot_d(y, th);
+}
+
+float circle(in vec2 st, in vec2 c, in float r) {
+  float d = length(st - c);
+  return plot_d(d, r);
 }
 
 #endif
