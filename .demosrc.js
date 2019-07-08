80ff0e1d38f84c7878772f0c14b1997cb460c37d
akira-cn <akira.cn@gmail.com>
2019-07-08 13:29:48 +0800

    update doc

---
+++ .demosrc.js
@@ -0,0 +1,34 @@
+module.exports = function(env) {
+  return {
+    devServer: {
+      port: 3000,
+    },
+    output: {
+      dir: 'docs/demo',
+      publicUrl: '.',
+    },
+    staticFolder: env === 'development' ? 'dist': null,
+    themeFile: 'demos/theme.scss',
+    demoList: env === 'development' ? '.demoList.dev.json': '.demoList.prod.json',
+    name: 'DOODLE',
+    version: 'v1',
+    homePage: 'http://doodle.webgl.group/',
+    logo: '',
+    // 可选主题: active4d, allHallowsEve, amy, blackboard, brillianceBlack,
+    // brillianceDull, chromeDevtools, cloudsMidnight, clouds, cobalt,
+    // dawn, dreamweaver, eiffel, espressoLibre, github, idle, katzenmilch,
+    // kuroirTheme, lazy, magicwbAmiga, merbivoreSoft, merbivore, monokai,
+    // pastelsOnDark, slushAndPoppies, solarizedDark, solarizedLight,
+    // spacecadet, sunburst, textmateMacClassic, tomorrowNightBlue,
+    // tomorrowNightBright, tomorrowNightEighties, tomorrowNight, tomorrow,
+    // twilight, vibrantInk, zenburnesque, iplastic, idlefingers, krtheme,
+    // monoindustrial,
+    boxTheme: 'monokai',
+    globalPackages: {
+      js: [env === 'development' ? '/glsl-doodle.js': '/js/glsl-doodle.js'],
+      css: [],
+    },
+    // tab waterfall
+    editorViewMode: 'tab',
+  };
+};
---
+++ demos/base.config.js
@@ -0,0 +1,39 @@
+import glsl from './glsl-lang.js';
+
+export default function (htmlCode, cssCode, jsCode) {
+  return {
+    javascript: {
+      code: jsCode,
+      transformer: 'glsl',
+      transform(code) {
+        return `const doodle = new Doodle(glslDoodle);
+doodle.compile(\`${code}\`).then((program) => {
+  doodle.useProgram(program);
+  doodle.render();
+});`;
+      },
+      editorHook(monaco, code, language) {
+        // Register a new language
+        monaco.languages.register({id: 'glsl'});
+
+        // Register a tokens provider for the language
+        monaco.languages.setMonarchTokensProvider('glsl', glsl);
+      },
+      visible: true,
+    },
+    html: {
+      code: htmlCode,
+      transformer: 'html',
+      visible: true,
+    },
+    css: {
+      code: cssCode,
+      transformer: 'css',
+    },
+    foldBoxes: ['html'],
+    packages: {
+      js: [],
+      css: [],
+    },
+  };
+}
