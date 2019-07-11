d5d6f4d2abf9ff094b3d8dffab83466ec825fbfc
akira-cn <akira.cn@gmail.com>
2019-07-11 19:04:17 +0800

    update doc

---
+++ doc-src/components/demoground.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class extends Component {
+  render() {
+    const iframeStyle = {
+      width: '100%',
+      height: '700px',
+      border: 'none',
+    };
+    return (
+      <iframe src="http://doodle.webgl.group/demo/#/coord2"
+        scrolling="yes"
+        style={iframeStyle}
+      ></iframe>
+    );
+  }
+}
